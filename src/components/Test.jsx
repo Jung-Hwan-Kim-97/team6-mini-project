cbd1675156754f12de4102af90a7e5c396877ddc
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-08-25 20:50:35 +0900

    feat: test-styled components

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import styled from 'styled-components'
 import { increase, decrease } from '../stores/testSlice'
 import { useTest } from '../stores/testSlice'
 const Test = () => {
@@ -14,12 +15,17 @@ const Test = () => {
   }
 
   return (
-    <>
+    <StyledTest>
       <div>{number}</div>
       <button onClick={increaseHandler}>+1</button>
       <button onClick={decreaseHandler}>-1</button>
-    </>
+    </StyledTest>
   )
 }
 
+const StyledTest = styled.div`
+  color: red;
+`
+
 export default Test
+
