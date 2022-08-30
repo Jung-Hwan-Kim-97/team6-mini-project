48722b07d27368afdf5ae02516cea9f7de704e1e
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-08-31 02:04:20 +0900

    refactor: Modify Favorite page

--- src/pages/Favorite/index.jsx
+++ src/pages/Favorite/index.jsx
@@ -1,62 +1,35 @@
-import React from 'react'
-import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
-import { Provider } from 'react-redux';
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
-import { favoriteHandler, useFavorite } from '../../stores/favoriteSlice';
-import { useProduct } from '../../stores/productSlice'
-import store from '../../stores/store';
-
-function ProductItem({ productList, favorite }) {
-  const { dispatch } = useFavorite();
-  return (
-    <div>
-      {productList.map((item, index) => (
-        <article className='card' key={index}>
-          <div>
-            <h3>{item.kor_co_nm}</h3>
-            <p>{item.fin_prdt_nm}</p>
-          </div>
-          <div className='favorite-icon' onClick={() => {dispatch(favoriteHandler(index))}}>
-            { favorite.favoriteList ? <AiFillHeart /> : <AiOutlineHeart />}
-          </div>
-        </article>
-      ))}
-    </div>
-  )
-}
+import FavoriteItem from '../../components/FavoriteItem';
+import { useFavorite } from '../../stores/favoriteSlice';
+import ProductItem from '../ProductList/ProductItem/ProductItem';
 
 function Favorite() {
-  const { productList } = useProduct();
-
   const { favorite } = useFavorite();
+  console.log(favorite)
 
   return (
-    <Provider store={store}>
-      <div>
-        {favorite.length === 0 ? <h3>관심상품을 추가해주세요</h3> : favorite}
-      </div>
-      <StyledProductList>
+    <StyledProductList>
         <section className="pageTitle">
-          <h1>상품안내</h1>
+          <h1>관심 상품</h1>
         </section>
-        <section className="actions">
-          <button className="btn">예금</button>
-          <button className="btn">적금</button>
-        </section>
-        <ProductItem productList={productList} favorite={favorite} />
-      </StyledProductList>
-    </Provider>
+        {favorite.length === 0 ? <h3>관심상품을 추가해주세요</h3> : favorite.map((item, index) => {
+          return <FavoriteItem key={index} item={item} favorite={favorite} />
+        })}
+    </StyledProductList>
   )
 }
 
 const StyledProductList = styled.div`
   margin: auto;
-  width: 400px;
-  /* border: 1px solid red; */
+  width: 1320px;
   height: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
   .pageTitle {
-    /* background-color: red; */
+    width: 100%;
     margin: 40px 0 30px 0;
     border-bottom: 2px solid #808080;
     box-sizing: border-box;
@@ -67,19 +40,21 @@ const StyledProductList = styled.div`
   }
   .actions {
     text-align: center;
-  }
-  .card {
-    background-color: #eee;
-    border: 1px solid #808080;
-    border-radius: 6px;
-    margin: 10px auto;
-    padding: 10px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    .favorite-icon {
-      cursor: pointer;
+
+    .btn {
+      outline: none;
+      border: none;
+      padding: 5px 40px;
+      margin: 10px;
+      background-color: ${({ theme }) => theme.palette.cobaltBlue};
+      color: #fff;
     }
+  }
+  .container {
+    width: 60%;
+
+    height: 400px;
+    overflow-y: auto;
   }
 `
 
