6dbd66eb1b41c2c5d1ac12c1976d4e5ededd22b6
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-08-31 19:20:44 +0900

    #5 feat: FavoriteButton structure

--- src/components/FavoriteButton.jsx
+++ src/components/FavoriteButton.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
+import { addFavorite, deleteFavorite, favoriteHandler, useFavorite } from '../stores/favoriteSlice';
+import styled from 'styled-components';
 
-function FavoriteButton() {
+function FavoriteButton({isFavorite, toggleFavorite, item}) {
+  const { dispatch, favorite } = useFavorite();
+
+  useEffect(() => {
+    console.log(isFavorite)
+    
+  },[])
+
   return (
-    <div>FavoriteButton</div>
+    <FavoriteBtn className="btn btn_favorite">
+      { isFavorite 
+        ? <AiFillHeart onClick={() => {
+            dispatch(deleteFavorite(item));
+            toggleFavorite();
+          }} /> 
+        : <AiOutlineHeart onClick={() => {
+            dispatch(addFavorite(item));
+            toggleFavorite();
+          }} />
+      }
+    </FavoriteBtn>
   )
 }
+
+const FavoriteBtn = styled.button`
+  .btn {
+    margin: 0 5px;
+    outline: none;
+    border: none;
+    padding: 5px 20px;
+    background-color: ${({ theme }) => theme.palette.cobaltBlue};
+    color: #fff;
+  }
+  .btn_favorite {
+    background-color: transparent;
+    padding: 0;
+  }
+`
 
 export default FavoriteButton
