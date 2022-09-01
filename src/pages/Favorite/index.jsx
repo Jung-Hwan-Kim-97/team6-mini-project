0f406dedb147843b3857579054b69b391496b7ae
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-09-01 16:26:35 +0900

    #5 design: Modify FavoritePage style

--- src/pages/Favorite/index.jsx
+++ src/pages/Favorite/index.jsx
@@ -1,60 +1,41 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import FavoriteItem from '../../components/FavoriteItem';
 import { useFavorite } from '../../stores/favoriteSlice';
-import ProductItem from '../ProductList/ProductItem/ProductItem';
 
 function Favorite() {
   const { favorite } = useFavorite();
   console.log(favorite)
 
   return (
-    <StyledProductList>
-        <section className="pageTitle">
-          <h1>관심 상품</h1>
+    <StyledFavoritePage>
+        <section className="page-title">
+          <h1 className='title-name'>관심 상품</h1>
         </section>
         {favorite.length === 0 ? <h3>관심상품을 추가해주세요</h3> : favorite.map((item, index) => {
-          return <FavoriteItem key={index} item={item} favorite={favorite} />
+          return <FavoriteItem key={index} item={item} />
         })}
-    </StyledProductList>
+    </StyledFavoritePage>
   )
 }
 
-const StyledProductList = styled.div`
+const StyledFavoritePage = styled.main`
   margin: auto;
-  width: 1320px;
-  height: 600px;
+  width: 100%;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  .pageTitle {
-    width: 100%;
+  background-color: ${({ theme }) => theme.palette.whiteColor};
+  .page-title {
+    width: 80%;
     margin: 40px 0 30px 0;
     border-bottom: 2px solid #808080;
     box-sizing: border-box;
-    h1 {
+    .title-name {
       font-size: 25px;
       margin-bottom: 20px;
     }
-  }
-  .actions {
-    text-align: center;
-
-    .btn {
-      outline: none;
-      border: none;
-      padding: 5px 40px;
-      margin: 10px;
-      background-color: ${({ theme }) => theme.palette.cobaltBlue};
-      color: #fff;
-    }
-  }
-  .container {
-    width: 60%;
-
-    height: 400px;
-    overflow-y: auto;
   }
 `
 
