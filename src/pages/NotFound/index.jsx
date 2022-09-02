8083c7be8a2ce5c3ce9f15a47394df907518c1a4
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-09-02 21:27:09 +0900

    #21 feat: Add NotFound Page structure

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import notfound from '../../assets/not-found.png'
 
 function NotFound() {
+  const navigate = useNavigate();
+
   return (
-    <div>NotFound</div>
+    <div>
+      <img src={notfound} alt="notfound-image" className='notfound-image' />
+      <button className='btn' onClick={() => {navigate("/")}}>메인페이지로 돌아가기</button>
+    </div>
   )
 }
 
